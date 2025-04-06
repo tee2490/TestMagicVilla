ff24ce89469866ea2df1fe42c0b4541091cec84e
Teeradet <tee2490@hotmail.com>
2025-04-06 15:57:35 +0700

    Section 6-3 Generic Type

--- MagicVilla_VillaAPI/Repository/IRepostiory/IVillaRepository.cs
+++ MagicVilla_VillaAPI/Repository/IRepostiory/IVillaRepository.cs
@@ -1,14 +1,8 @@
-﻿using System.Linq.Expressions;
-
-namespace MagicVilla_VillaAPI.Repository.IRepostiory
+﻿namespace MagicVilla_VillaAPI.Repository.IRepostiory
 {
-    public interface IVillaRepository
+    public interface IVillaRepository : IRepository<Villa>
     {
-        Task<List<Villa>> GetAllAsync(Expression<Func<Villa, bool>>? filter = null);
-        Task<Villa> GetAsync(Expression<Func<Villa, bool>>? filter = null, bool tracked = true);
-        Task CreateAsync(Villa entity);
-        Task UpdateAsync(Villa entity);
-        Task RemoveAsync(Villa entity);
-        Task SaveAsync();
+
+        Task<Villa> UpdateAsync(Villa entity);
     }
 }
