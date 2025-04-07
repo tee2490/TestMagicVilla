d52fc1a4ca65e3362cafe0b13edd3e21d5c07beb
Teeradet <tee2490@hotmail.com>
2025-04-07 14:45:47 +0700

    Section 7-8 Update Villa

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -45,5 +45,32 @@ namespace MagicVilla_Web.Controllers
             }
             return View(model);
         }
+
+        public async Task<IActionResult> UpdateVilla(int villaId)
+        {
+            var response = await _villaService.GetAsync<APIResponse>(villaId);
+            if (response != null && response.IsSuccess)
+            {
+                VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+                return View(_mapper.Map<VillaUpdateDTO>(model));
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateVilla(VillaUpdateDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _villaService.UpdateAsync<APIResponse>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(model);
+        }
+
     }
 }
