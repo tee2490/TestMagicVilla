a2f4888f0030c2b0dadb1651bd17503986ce463d
Teeradet <tee2490@hotmail.com>
2025-04-07 14:33:53 +0700

    Section 7-7 Create Villa

--- MagicVilla_Web/Controllers/VillaController.cs
+++ MagicVilla_Web/Controllers/VillaController.cs
@@ -24,5 +24,26 @@ namespace MagicVilla_Web.Controllers
             }
             return View(list);
         }
+
+        public async Task<IActionResult> CreateVilla()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var response = await _villaService.CreateAsync<APIResponse>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+            return View(model);
+        }
     }
 }
