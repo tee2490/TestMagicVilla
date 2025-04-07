8bf43ed0cc92a4e6241aa34a3da3197abb342045
Teeradet <tee2490@hotmail.com>
2025-04-07 15:38:29 +0700

    Section 7-12 Get Villa Numbers

---
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly IVillaNumberService _villaNumberService;
+        private readonly IMapper _mapper;
+
+        public VillaNumberController(IVillaNumberService villaNumberService, IMapper mapper)
+        {
+            _villaNumberService = villaNumberService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexVillaNumber()
+        {
+            List<VillaNumberDTO> list = new();
+
+            var response = await _villaNumberService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaNumberDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
