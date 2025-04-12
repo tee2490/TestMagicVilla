d784c3ba5fa8ccac58cd7923285488bceadcbe2b
Teeradet <tee2490@hotmail.com>
2025-04-12 11:28:27 +0700

    Section 9-1 Web : Add Auth Service

---
+++ MagicVilla_Web/Services/AuthService.cs
@@ -0,0 +1,35 @@
+﻿namespace MagicVilla_Web.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        private string villaUrl;
+
+        public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+        {
+            _clientFactory = clientFactory;
+            villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+
+        }
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/Users/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(RegisterationRequestDTO obj)
+        {
+            return SendAsync<T>(new APIRequest()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = obj,
+                Url = villaUrl + "/api/Users/register"
+            });
+        }
+    }
+}
---
+++ MagicVilla_Web/Services/IServices/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿namespace MagicVilla_Web.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T> LoginAsync<T>(LoginRequestDTO objToCreate);
+        Task<T> RegisterAsync<T>(RegisterationRequestDTO objToCreate);
+    }
+}
