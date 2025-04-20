b458a55b2752512427c78f1a6956e2f9a67136ee
Teeradet <tee2490@hotmail.com>
2025-04-20 14:35:39 +0700

    Section 14-10 Execute Update Feature

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -151,14 +151,9 @@ namespace MagicVilla_VillaAPI.Repository
             if (!existingRefreshToken.IsValid)
             {
                 var chainRecords = _db.RefreshTokens.Where(u => u.UserId == existingRefreshToken.UserId
-                && u.JwtTokenId == existingRefreshToken.JwtTokenId);
-
-                foreach (var item in chainRecords)
-                {
-                    item.IsValid = false;
-                }
-                _db.UpdateRange(chainRecords);
-                _db.SaveChanges();
+                && u.JwtTokenId == existingRefreshToken.JwtTokenId)
+                     .ExecuteUpdate(u => u.SetProperty(refreshToken => refreshToken.IsValid, false)); 
+                                
                 return new TokenDTO();
             }
 
